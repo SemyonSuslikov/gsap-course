05f3c40021b5821cbfa56c9e5bf57fd9a2f69a1a
Semyon Suslikov <semyonsuslikov@MacBook-Pro-Semyon.local>
2025-10-17 18:36:04 +0200

    Updated app.html and app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const firstName = 'Semyon';
+const lastName = 'Suslikov';
+const age = '27';
+const city = 'Krasnodar';
+
+console.log('Hello! My name is ' + firstName + ' ' + lastName + '. I\'m ' + age + ' years old. I\'m from ' + city + '.');
