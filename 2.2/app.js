a07775739d1e0be80d708c6cd3ff27f65736af43
Semyon Suslikov <semyonsuslikov@MacBook-Pro-Semyon.local>
2025-10-18 00:25:50 +0200

    update dz2.2s

---
+++ 2.2/app.js
@@ -0,0 +1,13 @@
+const titleProject = 'Сайт на Webflow';
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+const screenPrice = 1000;
+const percentage = 25;
+const fullPrice = 1500;
+const responsive = true;
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive)
